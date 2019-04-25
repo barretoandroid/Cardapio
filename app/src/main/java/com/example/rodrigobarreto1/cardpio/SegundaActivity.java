adb376618c2908e03e5ccb64f3044fc2df9e5a30
barretorodrigo <barrretorodrigo@gmail.com>
2019-04-25 11:47:41 -0300

    Lista de produtos dinâmica

--- app/src/main/java/com/example/rodrigobarreto1/cardpio/SegundaActivity.java
+++ app/src/main/java/com/example/rodrigobarreto1/cardpio/SegundaActivity.java
@@ -3,13 +3,17 @@ package com.example.rodrigobarreto1.cardpio;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.rodrigobarreto1.cardpio.modelos.Produto;
 
+import java.util.ArrayList;
+
 public class SegundaActivity extends AppCompatActivity {
 
-    public Produto produto1;
+    public ArrayList<Produto> produtos = new ArrayList<>();
+    public LinearLayout layoutLinear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,21 +26,48 @@ public class SegundaActivity extends AppCompatActivity {
         TextView textViewTitulo = findViewById(R.id.tvTitulo);
         textViewTitulo.setText(titulo);
 
-        TextView textViewItem1 = findViewById(R.id.tvItem1);
-        TextView textViewDesc1 = findViewById(R.id.tvDesc1);
-        TextView textViewPreco1 = findViewById(R.id.tvPreco1);
 
         if (titulo.equals("Berinjela")){
-            produto1 = new Produto("Antepasto", 12.99,  "Berinjela", "Berinjela, pimentão, azeitona e cebola");
-            textViewItem1.setText(produto1.getNome());
-            textViewDesc1.setText(produto1.getDescricao());
-            textViewPreco1.setText("R$ " + produto1.getPreco().toString());
+            produtos.add(new Produto("Antepasto", 12.99,  "Berinjela", "Berinjela, pimentão, azeitona e cebola"));
+            produtos.add(new Produto("Parmegiana", 17.99,  "Berinjela", "Berinjela, pimentão, azeitona e cebola"));
+            produtos.add(new Produto("Recheada", 14.99,  "Berinjela", "Berinjela, pimentão, azeitona e cebola"));
         }
 
         else if (titulo.equals("Hamburguer")){
-
-            textViewItem1.setText("X-Salada");
+            produtos.add(new Produto("X-Salada", 14.99,  "Hamburguer", "Queijo, hamburguer, alface e tomate"));
+            produtos.add(new Produto("X-Egg", 12.99,  "Hamburguer", "Queijo, hambuguer e ovo"));
         }
 
-    }
-}
+        else if (titulo.equals("Drinks")){
+            produtos.add(new Produto("Caipirinha", 9.99,  "Drinks", "Suco de limão e açucar"));
+        }
+
+        else if (titulo.equals("Doces")){
+            produtos.add(new Produto("Pudim", 99.99,  "Doces", "Uma delícia"));
+        }
+
+        listaProdutos(produtos);
+
+    }//Método onCreate
+
+    public void listaProdutos(ArrayList<Produto> lista){
+
+        layoutLinear = findViewById(R.id.lVertical);
+        for (int i=0; i<lista.size(); i++) {
+            TextView textViewItem1 = new TextView(this);
+            TextView textViewDesc1 = new TextView(this);
+            TextView textViewPreco1 = new TextView(this);
+
+            textViewItem1.setText(lista.get(i).getNome());
+            textViewItem1.setTextSize(18);
+            textViewDesc1.setText(lista.get(i).getDescricao());
+            textViewPreco1.setText("R$ " + lista.get(i).getPreco().toString());
+
+            layoutLinear.addView(textViewItem1);
+            layoutLinear.addView(textViewDesc1);
+            layoutLinear.addView(textViewPreco1);
+        }
+
+    }//Método listaProdutos
+
+}//Final da Classe
