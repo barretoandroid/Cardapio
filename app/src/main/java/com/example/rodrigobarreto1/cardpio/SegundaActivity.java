6394aab276708cb8de0829fef379c5ed850ca930
barretorodrigo <barrretorodrigo@gmail.com>
2019-04-25 09:54:45 -0300

    Desenvolvimento da classe produtos e adição de ícone

--- app/src/main/java/com/example/rodrigobarreto1/cardpio/SegundaActivity.java
+++ app/src/main/java/com/example/rodrigobarreto1/cardpio/SegundaActivity.java
@@ -5,7 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.rodrigobarreto1.cardpio.modelos.Produto;
+
 public class SegundaActivity extends AppCompatActivity {
+
+    public Produto produto1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,13 +18,25 @@ public class SegundaActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String titulo = intent.getStringExtra(MainActivity.TITULO);
-        String item1 = intent.getStringExtra(MainActivity.ITEM1);
-        String item2 = intent.getStringExtra(MainActivity.ITEM2);
+
         TextView textViewTitulo = findViewById(R.id.tvTitulo);
-        TextView textViewItem1 = findViewById(R.id.tvItem1);
-        TextView textViewItem2 = findViewById(R.id.tvItem2);
         textViewTitulo.setText(titulo);
-        textViewItem1.setText(item1);
-        textViewItem2.setText(item2);
+
+        TextView textViewItem1 = findViewById(R.id.tvItem1);
+        TextView textViewDesc1 = findViewById(R.id.tvDesc1);
+        TextView textViewPreco1 = findViewById(R.id.tvPreco1);
+
+        if (titulo.equals("Berinjela")){
+            produto1 = new Produto("Antepasto", 12.99,  "Berinjela", "Berinjela, pimentão, azeitona e cebola");
+            textViewItem1.setText(produto1.getNome());
+            textViewDesc1.setText(produto1.getDescricao());
+            textViewPreco1.setText("R$ " + produto1.getPreco().toString());
+        }
+
+        else if (titulo.equals("Hamburguer")){
+
+            textViewItem1.setText("X-Salada");
+        }
+
     }
 }
